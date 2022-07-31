e83b2932f0f0120c7f8728a9acfa775038f5792d
Junior Dering <juniordering@hotmail.com>
2022-07-30 20:24:53 -0400

    adicionando cors

--- pages/api/questionario/index.ts
+++ pages/api/questionario/index.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express'
+import NextCors from 'nextjs-cors';
 import { embaralhar } from '../../../helpers'
 import questoes from '../BDQuestoes'
-export default function Questionario(req: Request,res: Response) {
+// @ts-ignore
+export default async function Questionario(req,res ) {
+    await NextCors(req, res, {
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+     });
     let ids = questoes.map(element=>element.id)
     res.status(200).json(embaralhar(ids))
 }
